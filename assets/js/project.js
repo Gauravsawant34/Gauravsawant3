5812e4d975ae1992f907ac539711f669ef5038ed
Gauravsawant34 <gauravsawant3198@gmail.com>
2023-04-24 16:02:06 +0530

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -7,7 +7,7 @@ const projectcards = document.querySelector(".projectcards");
 const projects = [
   {
     title: "Survey Form",
-    cardImage: "assets/images/project-page/quiz.jpg",
+    cardImage: "assets/images/project-page/form.jpeg",
     description: "A survey form app built using HTML, CSS and web designing skills.",
     tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
     Previewlink: "",
@@ -15,7 +15,7 @@ const projects = [
   },
   {
     title: "ZanCat",
-    cardImage: "assets/images/project-page/exercise.jpg",
+    cardImage: "assets/images/project-page/zancat.jpg",
     description: "ZnCat website built using basic html,css,bootstrap.It has a clean and modern design,with a smple and easy-to-navigate user interface.",
     tagimg: "https://miro.medium.com/max/2800/0*U2DmhXYumRyXH6X1.png",
     Previewlink: " https://gauravsawant34.github.io/ZanCat-Website/",
@@ -32,7 +32,7 @@ const projects = [
   },
   {
     title: "Musicca Drumkit Engine",
-    cardImage: "assets/images/project-page/music.jpg",
+    cardImage: "assets/images/project-page/musiccadrumkit.jpg",
     description: "The Musicca Drum kit project is  a fun  and interactive web developement project that allows leraners to Practice their HtML cSS ,and Javascript skills by creating a Virtual Drum kit.",
     tagimg:
       "https://img.freepik.com/premium-vector/drum-set-music-cartoon-icon-illustration-music-instrument-icon-concept-isolated-premium-flat-cartoon-style_138676-1601.jpg?w=740",
