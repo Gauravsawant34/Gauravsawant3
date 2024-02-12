74d2c4a7c87cf365bd76ff5e787b3397149586f3
Gauravsawant34 <gauravsawant3198@gmail.com>
2024-02-13 02:20:11 +0530

    Update education.js

--- assets/js/education.js
+++ assets/js/education.js
@@ -167,20 +167,20 @@ $(function () {
     });
   }
 
- // sr.reveal(".js--fadeInLeft", {
- //    origin: "left",
- //    distance: "300px",
- //    easing: "ease-in-out",
- //    duration: 800,
- //  });
+ sr.reveal(".js--fadeInLeft", {
+    origin: "left",
+    distance: "300px",
+    easing: "ease-in-out",
+    duration: 800,
+  });
   
 
- //  sr.reveal(".js--fadeInRight", {
- //    origin: "right",
- //    distance: "300px",
- //    easing: "ease-in-out",
- //    duration: 800,
- //  });
+  sr.reveal(".js--fadeInRight", {
+    origin: "right",
+    distance: "300px",
+    easing: "ease-in-out",
+    duration: 800,
+  });
   
   
   
